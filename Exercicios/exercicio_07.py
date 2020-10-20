6b2508639abba629fc25015259c75437f32cdd60
Matheus Ganzala Nunes Teixeira <45406327+MagicGanza@users.noreply.github.com>
2020-10-20 10:04:19 -0300

    Update exercicio_07.py

--- Exercicios/exercicio_07.py
+++ Exercicios/exercicio_07.py
@@ -4,10 +4,11 @@ Exercícios 07 do curso da Udemy;
 Fazer um programa para receber duas notas, calcular a média se forém verdadeiras (de 0.0 a 10.0), se
  não forem verdareiras, imprimir na tela
 """
-print('Digite a nota um')
-n1 = float(input())
+n1 = float(input('Digite a nota 1  '))
+n2 = float(input('Digite a nota 2  '))
 
-print('Digite a nota dois')
-n2 = float(input())
-
-print(f'A média é {n1 + n2 / 2}')
+if n1 <= 10 and n1 >= 0 and n2 <= 10 and n2 >= 0:
+    media = (n1 + n2) / 2
+    print(f'A média é {media}')
+else:
+    print('Digite notas válidas...')
