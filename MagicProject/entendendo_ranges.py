182ef7ba43ff13aa8d44a12759e55ed57e0ffcfc
Matheus Ganzala Nunes Teixeira <45406327+MagicGanza@users.noreply.github.com>
2020-10-20 12:21:41 -0300

    Add files via upload

---
+++ MagicProject/entendendo_ranges.py
@@ -0,0 +1,45 @@
+"""
+Ranges
+
+- Precisamos conhecer o loop for para usar os ranges.
+- Precisamos conhecer o range para trabalhar melhor com loops for.
+
+Ranges são utilizados para gerar sequencias numéricas, não de forma aleatória,
+
+Formas gerais:
+
+range(valor_de_parada)
+
+Obs: valor_de_parada não inclusive (início padrão 0, e passo de 1 em 1)
+
+# Exemplo forma 1
+
+for num in range(11):
+    print(num)
+
+# Exemplo forma 2
+range(valor_de_inicio, valor_de_parada)
+
+for num in range(4, 11):
+    print(num)
+
+Obs: valor_de_parada não inclusive (ínicio especificado pelo usuário, e passo de 1 em 1)
+
+# Exemplo forma 3
+range(valor_de_inicio, valor_de_parada, passo)
+
+for num in range(5, 55, 5):
+    print(num)
+
+Obs: valor_de_parada não inclusive (ínicio especificado pelo usuário, e passo especificado pelo usuário)
+
+# Exemplo forma 4 (Inversa)
+range(valor_de_inicio, valor_de_parada, passo)
+
+for num in range(10, -1, -1):
+    print(num)
+
+Obs: valor_de_parada não inclusive(ínicio especificado pelo usuário, e passo especificado pelo usuário)
+"""
+)
+
---
+++ MagicProject/loop_while.py
@@ -0,0 +1,47 @@
+"""
+Loop while
+
+fOrma geral
+
+while expressão_booleana:
+    //execução do loop
+
+o bloco do wile será repetido enquando a expressão booleana for verdadeira.
+
+Expressçao Booleana é toda expressão onde o resultado é verdadeiro ou falso
+
+Exemplo:
+
+num = 5
+
+num < 5
+
+numero = 1
+
+# Exemplo 1
+
+while numero < 10:
+    print(numero)
+    # numero = numero + 1
+
+# Obs em um loop while, é importante que cuidemos do critério de parada para não causar um loop infinito
+
+
+# Java ou C
+ while(expressão)
+ {
+ //execução
+ }
+
+ do
+ {
+ }while(expressão);
+"""
+
+# Exemplo 2
+
+resposta = ''
+
+while resposta != 'sim':
+    respsota = input('Já acabou Jéssica ?')
+
